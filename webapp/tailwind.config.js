c6604d226ccbf751b45d62ed9d3045f26d5c3548
Sri <sri17mahathi@gmail.com>
2025-06-29 14:42:41 -0400

    fix

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -2,15 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}",],
   theme: {
-    extend: {
-      colors: {
-        lavender: '#BFA2DB',
-        'lavender-dk': '#C9BBCF',
-        floral: '#F6E6FA',
-        deep: '#4B4453',
-        muted: '#DADADA',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
