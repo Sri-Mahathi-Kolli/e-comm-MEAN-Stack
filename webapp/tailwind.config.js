da0164a682a44252d348b1ac4e2e6c09cae16797
Sri <sri17mahathi@gmail.com>
2025-06-29 12:36:35 -0400

    dailyfix1

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lavender: '#BFA2DB',
+        'lavender-dk': '#C9BBCF',
+        floral: '#F6E6FA',
+        deep: '#4B4453',
+        muted: '#DADADA',
+      },
+    },
   },
   plugins: [],
 }
